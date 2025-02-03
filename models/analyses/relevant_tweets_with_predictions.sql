fb91bbe15be8428f48fd1ce142ae508e7c521104
Sophie <zetta@MacBook-Air-von-Sophie-2.local>
2025-02-03 21:55:04 +0100

    working version

--- models/analyses/relevant_tweets_with_predictions.sql
+++ models/analyses/relevant_tweets_with_predictions.sql
@@ -55,33 +55,54 @@ politician_data AS (
         gender,
         year_of_birth
     FROM {{ ref('02_politically_engaged_users') }}
-), final AS (
+),
 
-SELECT
-    td.tweet_id,
-    td.user_id,
-    users.screen_name,
-    users.lang,
-    users.location,
-    users.name,
-    td.created_at,
-    td.refers_to_user_id,
-    td.refers_to_tweet_id,
-    td.mentioned_ids,
-    td.hashtags,
-    td.text,
-    st.toxicity_score,
-    st.sentiment_score,
-    pd.party,
-    pd.office,
-    pd.institution,
-    pd.region,
-    pd.gender,
-    pd.year_of_birth
-FROM relevant_tweets AS td
-LEFT JOIN sentiment_toxicity AS st ON td.tweet_id = st.tweet_id
-LEFT JOIN politician_data AS pd ON td.user_id = pd.user_id
-LEFT JOIN users AS users ON td.user_id = users.user_id
+hashtag_categories AS (
+    SELECT
+        LOWER(hashtag) AS hashtag,
+        ARRAY_AGG(DISTINCT category) AS categories
+    FROM {{ ref('hashtag_categories_exploded') }}
+    GROUP BY hashtag
+),
+
+tweet_categories AS (
+    SELECT
+        td.tweet_id,
+        ARRAY_CONCAT_AGG(hc.categories) AS aggregated_categories
+    FROM relevant_tweets AS td
+    CROSS JOIN UNNEST(td.hashtags) AS hashtag
+    LEFT JOIN hashtag_categories AS hc ON LOWER(hashtag) = hc.hashtag
+    GROUP BY td.tweet_id
+),
+
+final AS (
+    SELECT
+        td.tweet_id,
+        td.user_id,
+        u.screen_name,
+        u.lang,
+        u.location,
+        u.name,
+        td.created_at,
+        td.refers_to_user_id,
+        td.refers_to_tweet_id,
+        td.mentioned_ids,
+        td.hashtags,
+        td.text,
+        st.toxicity_score,
+        st.sentiment_score,
+        pd.party,
+        pd.office,
+        pd.institution,
+        pd.region,
+        pd.gender,
+        pd.year_of_birth,
+        tc.aggregated_categories
+    FROM relevant_tweets AS td
+    LEFT JOIN sentiment_toxicity AS st ON td.tweet_id = st.tweet_id
+    LEFT JOIN politician_data AS pd ON td.user_id = pd.user_id
+    LEFT JOIN users AS u ON td.user_id = u.user_id
+    LEFT JOIN tweet_categories AS tc ON td.tweet_id = tc.tweet_id
 )
 
 SELECT * 
